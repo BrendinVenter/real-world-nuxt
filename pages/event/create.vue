5204bff2c8d8b69ca00a36e518fd0939ae72e0ef
Brendin Venter <brendincolinventer@gmail.com>
2020-10-07 14:11:57 +0400

    project updates

---
+++ pages/event/create.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Create a new event for your area',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
